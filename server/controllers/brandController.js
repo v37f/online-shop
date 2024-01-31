34764c05b48a663366ab44a877e3bd809e602e24
Vladimir Frolov <frolov_vo@inbox.ru>
2024-01-31 19:30:41 +0300

    feat: add check role middleware

--- server/controllers/brandController.js
+++ server/controllers/brandController.js
@@ -2,10 +2,14 @@ const { Brand } = require('../models/models');
 const ApiError = require('../error/ApiError');
 
 class BrandController {
-  async create(req, res) {
+  async create(req, res, next) {
     const { name } = req.body;
-    const brand = await Brand.create({ name });
-    return res.json(brand);
+    try {
+      const brand = await Brand.create({ name });
+      return res.json(brand);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
   }
 
   async getAll(req, res) {
--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -2,10 +2,14 @@ const { Type } = require('../models/models');
 const ApiError = require('../error/ApiError');
 
 class TypeController {
-  async create(req, res) {
+  async create(req, res, next) {
     const { name } = req.body;
-    const type = await Type.create({ name });
-    return res.json(type);
+    try {
+      const type = await Type.create({ name });
+      return res.json(type);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
   }
 
   async getAll(req, res) {
