e6d1a5147d4c9a0d1909f4ab676b4dfad3ee3eb0
Vladimir Frolov <frolov_vo@inbox.ru>
2024-02-01 23:38:49 +0300

    feat: add device page markup

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,7 +1,81 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import bigStar from '../assets/bigStar.png';
 
 const DevicePage = () => {
-  return <div>DevicePage</div>;
+  const device = {
+    id: 6,
+    name: 'Iphone 12 pro',
+    price: 25000,
+    rating: 5,
+    img: 'https://www.yablokitradein.ru/image/cache/catalog/12%20pro%20iphone/iphone-12-pro-blue-hero-500x500.png',
+  };
+  const description = [
+    { id: 1, title: 'Оперативная память', description: '5 гб' },
+    { id: 2, title: 'Камера', description: '12 мп' },
+    { id: 3, title: 'Процессор', description: 'Пентиум 3' },
+    { id: 4, title: 'Кол-во ядер', description: '2' },
+    { id: 5, title: 'Аккумулятор', description: '4000' },
+  ];
+  return (
+    <Container className="mt-3">
+      <Row>
+        <Col md={4}>
+          <Image width={300} height={300} src={device.img} />
+        </Col>
+        <Col md={4}>
+          <Row className="d-flex flex-column align-items-center">
+            <h2 style={{ maxWidth: 'fit-content' }}>{device.name}</h2>
+            <div
+              className="d-flex align-items-center justify-content-center"
+              style={{
+                background: `url(${bigStar}) no-repeat center center`,
+                width: 240,
+                height: 240,
+                backgroundSize: 'cover',
+                fontSize: 64,
+              }}
+            >
+              {device.rating}
+            </div>
+          </Row>
+        </Col>
+        <Col md={4}>
+          <Card
+            className="d-flex flex-column align-items-center justify-content-around"
+            style={{
+              width: 300,
+              height: 300,
+              fontSize: 32,
+              border: '5px solid lightgray',
+            }}
+          >
+            <h3>От: {device.price} руб.</h3>
+            <Button variant={'outline-dark'}>Добавить в корзину</Button>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="d-flex flex-column m-3">
+        <h1>Характеристики</h1>
+        {description.map((info, index) => (
+          <Row
+            key={info.id}
+            style={{
+              background: index % 2 === 0 ? 'lightgray' : 'transparent',
+              padding: 10,
+            }}
+          >
+            {info.title} : {info.description}
+          </Row>
+        ))}
+      </Row>
+    </Container>
+  );
 };
 
 export default DevicePage;
