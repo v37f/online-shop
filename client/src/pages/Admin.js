8e953add7a150d85f475ceb8ba4b089fa354e58d
Vladimir Frolov <frolov_vo@inbox.ru>
2024-02-02 20:58:33 +0300

    feat: add admin modals

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import CreateBrand from '../components/modals/CreateBrand';
+import CreateType from '../components/modals/CreateType';
+import CreateDevice from '../components/modals/CreateDevice';
 
 const Admin = () => {
+  const [brandVisible, setBrandVisible] = useState(false);
+  const [typeVisible, setTypeVisible] = useState(false);
+  const [deviceVisible, setDeviceVisible] = useState(false);
+
   return (
     <Container className="d-flex flex-column">
-      <Button variant={'outline-dark'} className="mt-4 p-2">
+      <Button variant={'outline-dark'} className="mt-4 p-2" onClick={() => setTypeVisible(true)}>
         Добавить тип
       </Button>
-      <Button variant={'outline-dark'} className="mt-4 p-2">
+      <Button variant={'outline-dark'} className="mt-4 p-2" onClick={() => setBrandVisible(true)}>
         Добавить бренд
       </Button>
-      <Button variant={'outline-dark'} className="mt-4 p-2">
+      <Button variant={'outline-dark'} className="mt-4 p-2" onClick={() => setDeviceVisible(true)}>
         Добавить устройство
       </Button>
+      <CreateBrand show={brandVisible} onHide={() => setBrandVisible(false)} />
+      <CreateType show={typeVisible} onHide={() => setTypeVisible(false)} />
+      <CreateDevice show={deviceVisible} onHide={() => setDeviceVisible(false)} />
     </Container>
   );
 };
