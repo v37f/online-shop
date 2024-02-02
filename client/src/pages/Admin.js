461229f336a610488b2aa9e91717453ae357f90a
Vladimir Frolov <frolov_vo@inbox.ru>
2024-02-02 20:20:33 +0300

    feat: add admin panel buttons

--- client/src/pages/Admin.js
+++ client/src/pages/Admin.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 const Admin = () => {
-  return <div>Admin page</div>;
+  return (
+    <Container className="d-flex flex-column">
+      <Button variant={'outline-dark'} className="mt-4 p-2">
+        Добавить тип
+      </Button>
+      <Button variant={'outline-dark'} className="mt-4 p-2">
+        Добавить бренд
+      </Button>
+      <Button variant={'outline-dark'} className="mt-4 p-2">
+        Добавить устройство
+      </Button>
+    </Container>
+  );
 };
 
 export default Admin;
