1ed09055d6e84ef49f1850f24dd853f74c5920d2
Vladimir Frolov <frolov_vo@inbox.ru>
2024-02-04 22:08:10 +0300

    feat: add create type and brand handler to admin panel

--- client/src/components/modals/CreateBrand.js
+++ client/src/components/modals/CreateBrand.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { createBrand, createType } from '../../http/deviceAPI';
 
 const CreateBrand = ({ show, onHide }) => {
+  const [value, setValue] = useState('');
+  const addBrand = () => {
+    createBrand({ name: value }).then((data) => {
+      setValue('');
+      onHide();
+    });
+  };
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Добавить бренд
-        </Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Добавить бренд</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control placeholder={'Введите название бренда'} />
+          <Form.Control
+            placeholder={'Введите название бренда'}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
         </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant={'outline-danger'} onClick={onHide}>
           Закрыть
         </Button>
-        <Button variant={'outline-success'} onClick={onHide}>
+        <Button variant={'outline-success'} onClick={addBrand}>
           Добавить
         </Button>
       </Modal.Footer>
--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { createType } from '../../http/deviceAPI';
 
 const CreateType = ({ show, onHide }) => {
+  const [value, setValue] = useState('');
+  const addType = () => {
+    createType({ name: value }).then((data) => {
+      setValue('');
+      onHide();
+    });
+  };
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
       <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Добавить тип
-        </Modal.Title>
+        <Modal.Title id="contained-modal-title-vcenter">Добавить тип</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control placeholder={'Введите название типа'} />
+          <Form.Control
+            placeholder={'Введите название типа'}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
         </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant={'outline-danger'} onClick={onHide}>
           Закрыть
         </Button>
-        <Button variant={'outline-success'} onClick={onHide}>
+        <Button variant={'outline-success'} onClick={addType}>
           Добавить
         </Button>
       </Modal.Footer>
