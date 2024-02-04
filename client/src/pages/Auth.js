414ab084d0931cc7dad4b322cc9af2e52693f73b
Vladimir Frolov <frolov_vo@inbox.ru>
2024-02-04 21:03:41 +0300

    feat: add reg, login and check handlers

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
-import { NavLink, useLocation } from 'react-router-dom';
-import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/constants';
+import { NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE, SHOP_ROUTE } from '../utils/constants';
+import { registration, login } from '../http/userAPI';
+import { observer } from 'mobx-react-lite';
+import { Context } from '../index';
 
-const Auth = () => {
+const Auth = observer(() => {
+  const { user } = useContext(Context);
   const location = useLocation();
   const isLogin = location.pathname === LOGIN_ROUTE;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const click = async () => {
+    try {
+      let data;
+      if (isLogin) {
+        data = await login(email, password);
+      } else {
+        data = await registration(email, password);
+      }
+      user.setUser(data);
+      user.setIsAuth(true);
+      navigate(SHOP_ROUTE);
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  };
 
   return (
     <Container
@@ -19,20 +42,30 @@ const Auth = () => {
       <Card style={{ width: 600 }} className="p-5">
         <h2 className="m-auto">{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
         <Form className="d-flex flex-column">
-          <Form.Control className="mt-3" placeholder="Введите ваш email..." />
-          <Form.Control className="mt-3" placeholder="Введите ваш пароль..." />
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите ваш email..."
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Form.Control
+            className="mt-3"
+            placeholder="Введите ваш пароль..."
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
           <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
             {isLogin ? (
               <div>
-                Нет аккаунта?{' '}
-                <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйтесь!</NavLink>
+                Нет аккаунта? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйтесь!</NavLink>
               </div>
             ) : (
               <div>
                 Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
               </div>
             )}
-            <Button className="mt-3 align-self-end" variant={'outline-success'}>
+            <Button className="mt-3 align-self-end" variant={'outline-success'} onClick={click}>
               {isLogin ? 'Войти' : 'Регистрация'}
             </Button>
           </Row>
@@ -40,6 +73,6 @@ const Auth = () => {
       </Card>
     </Container>
   );
-};
+});
 
 export default Auth;
