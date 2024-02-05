e8838cd34b5e4ed0e248bc04c1d75e6557c3a17a
Vladimir Frolov <frolov_vo@inbox.ru>
2024-02-05 21:57:29 +0300

    feat: add pagination to shop

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -8,6 +8,7 @@ import DeviceList from '../components/DeviceList';
 import { observer } from 'mobx-react-lite';
 import { Context } from '../index';
 import { fetchBrands, fetchTypes, fetchDevices } from '../http/deviceAPI';
+import Pages from '../components/Pages';
 
 const Shop = observer(() => {
   const { device } = useContext(Context);
@@ -15,8 +16,18 @@ const Shop = observer(() => {
   useEffect(() => {
     fetchTypes().then((data) => device.setTypes(data));
     fetchBrands().then((data) => device.setBrands(data));
-    fetchDevices().then((data) => device.setDevices(data.rows));
+    fetchDevices(null, null, 1, 2).then((data) => {
+      device.setDevices(data.rows);
+      device.setTotalCount(data.count);
+    });
   }, []);
+
+  useEffect(() => {
+    fetchDevices(device.selectedType.id, device.selectedBrand.id, device.page, 2).then((data) => {
+      device.setDevices(data.rows);
+      device.setTotalCount(data.count);
+    });
+  }, [device.page, device.selectedType.id, device.selectedBrand.id]);
 
   return (
     <Container>
@@ -27,6 +38,7 @@ const Shop = observer(() => {
         <Col md={9}>
           <BrandBar />
           <DeviceList />
+          <Pages />
         </Col>
       </Row>
     </Container>
